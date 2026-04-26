eeae72dc3ccd15749f887ea4ee4e23268c523c9a
BrunciTunci <djurojaksic@gmail.com>
2026-04-26 16:31:17 +0200

    Update Program.cs

--- FinSight.Api/Program.cs
+++ FinSight.Api/Program.cs
@@ -77,7 +77,10 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseCors(corsPolicyName);
 
